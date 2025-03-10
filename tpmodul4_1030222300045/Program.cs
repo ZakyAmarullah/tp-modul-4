280054049799ec6cbe69ed9fca721c825c13ee70
ZakyAmarullah <zakym0208@gmail.com>
2025-03-10 12:02:14 +0700

    menambahkan implementasi dengan state-based construction

--- tpmodul4_1030222300045/Program.cs
+++ tpmodul4_1030222300045/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 public class KodePos
 {
@@ -29,3 +30,84 @@ public class KodePos
         return "Kodepos tidak ditemukan";
     }
 };
+
+public class DoorMachine
+{
+    enum State { Terkunci, Terbuka, Keluar};
+    public void Door()
+    {
+        State state = State.Terkunci;
+        String[] status = { "Terkunci", "Tebuka", "Keluar" };
+        while (state != State.Keluar)
+        {
+            Console.WriteLine("\n" + "Pintu " + status[(int)state] + "\n");
+            Console.WriteLine("Masukkan perintah (ketik 'Keluar' untuk kembali ke menu) : ");
+            String command = Console.ReadLine();
+
+            switch (state)
+            {
+                case State.Terkunci:
+                    if (command == "Terbuka")
+                    {
+                        state = State.Terbuka;
+                    }
+                    else if(command == "Keluar")
+                    {
+                        Program.Main();
+                    }
+                    break;
+                case State.Terbuka:
+                    if (command == "Terkunci")
+                    {
+                        state = State.Terkunci;
+                    }
+                    else if(command == "Keluar")
+                    {
+                        Program.Main();
+                    }
+                    break;
+            }   
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("1. Program kodepos");
+        Console.WriteLine("2. Program Door Machine");
+        Console.WriteLine("0. Exit");
+        Console.Write("Masukkan pilihan Anda: ");
+
+        String masuk = Console.ReadLine();
+        while (masuk != "0")
+        {
+            if (masuk == "1")
+            {
+                KodePos kode = new KodePos();
+                Console.Write("Masukkan nama Kelurahan (ketik 'Kembali' untuk balik ke menu) : ");
+                string Kelurahan = Console.ReadLine() ?? "";
+
+                if (Kelurahan != "Kembali")
+                {
+                    string Kode = KodePos.getKodepos(Kelurahan);
+                    Console.WriteLine($"Kodepos {Kelurahan} : {Kode}");
+                }
+                else
+                {
+                    Main();
+                }
+            }
+            else if(masuk == "2")
+            {
+                DoorMachine door = new DoorMachine();
+                door.Door();
+            }
+            else if(masuk == "0")
+            {
+                break;
+            }
+        }
+    }
+}
